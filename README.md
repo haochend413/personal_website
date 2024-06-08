d4ce5f8da9a82c05752c344d782118a8feddb200
haochend413 <dinghaochen413@gmail.com>
2024-06-08 08:23:58 -0500

    implemented package.json

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyWebpage
-This is my personal webpage. I use this as front-end learning practice. 
+
+This is my personal webpage. I use this as front-end learning practice.
---
+++ src/index.js
@@ -0,0 +1,7 @@
+//where everything is connected to local browser.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+// renders React Component "Root" into the DOM element with ID "root"
+ReactDOM.render(<App />, document.getElementById("root"));
