06b0145a4ccbd29caea8471e9dc70e4cf2637e20
haochend413 <dinghaochen413@gmail.com>
2024-11-03 16:01:01 -0600

    changed resume link again

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export default function App() {
           <Resumebutton setpage={setpage} />
           <h1>Resume & Websites</h1>
           <p className="box">
-            <a href="blob/main/public/Resume.pdf">Resume</a>
+            <a href="tree/main/public/Resume.pdf">Resume</a>
             <br></br>
             <a href="https://github.com/haochend413">Github</a>
             <br></br>
