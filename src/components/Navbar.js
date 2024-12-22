2cb871c5c666c0970d39f3b78008c6fac154de44
haochend413 <dinghaochen413@gmail.com>
2024-12-22 01:10:31 -0600

    minor fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,11 @@ function Navbar() {
   const navigate = useNavigate();
   return (
     <nav>
-      <ul>
-        <button onClick={() => navigate("/")}>HOME</button>
-        <button onClick={() => navigate("/about")}>ABOUT</button>
-        <button onClick={() => navigate("/experiences")}>EXPERIENCES</button>
-        <button onClick={() => navigate("/resume")}>RESUME</button>
-        {/* Add more links as needed */}
-      </ul>
+      <button onClick={() => navigate("/")}>HOME</button>
+      <button onClick={() => navigate("/about")}>ABOUT</button>
+      <button onClick={() => navigate("/experiences")}>EXPERIENCES</button>
+      <button onClick={() => navigate("/resume")}>RESUME</button>
+      {/* Add more links as needed */}
     </nav>
   );
 }
